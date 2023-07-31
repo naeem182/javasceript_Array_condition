c74e4401119f359b2d74e34f1311098fe2b51fe0
naeem <naeemislamjr182@gmail.com>
2023-07-31 22:31:29 +0600

    array assign

---
+++ array.js
@@ -0,0 +1,9 @@
+var friend_age = [20, 21, 22, 15];
+
+console.log(typeof friend_age)
+console.log(friend_age)
+var nayikas = ["mahi", "sabnur",]
+console.log(nayikas)
+//nayikas length
+console.log(nayikas.length)
+console.log(friend_age.length)
