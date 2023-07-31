1969da7fcbdc249ce9ebbbc225117ec1e92685c4
naeem <naeemislamjr182@gmail.com>
2023-07-31 23:26:34 +0600

    arraypush pop set find index

---
+++ arrayindex.js
@@ -0,0 +1,29 @@
+var friend_age = [20, 21, 22, 15, 19];
+
+//get element index
+console.log(friend_age[3])
+
+var nayikas = ["mahi", "sabnur",]
+console.log(nayikas[0])
+console.log(nayikas)
+
+//push mane shesh e add hbe
+nayikas.push("jorina")
+console.log(nayikas)
+nayikas.push("sokina")
+console.log(nayikas)
+nayikas.push("sumaiya")
+//after push
+console.log(nayikas)
+
+//pop mne shesh theke remove haha gayeb hye jbe
+nayikas.pop()
+console.log(nayikas)
+//set index num value ager ta chole jabe var[]=
+nayikas[2] = "bubli"
+console.log(nayikas)
+//find index value with indexof()
+nayikas.indexOf("bubli")
+console.log(nayikas.indexOf("bubli"))
+nayikas.indexOf("mahi")
+console.log(nayikas.indexOf("mahi"))
