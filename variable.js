db1065b828e2a88d119d583286b6009e0ebc7ec1
naeem <naeemislamjr182@gmail.com>
2023-07-31 21:34:13 +0600

    varriable review

---
+++ variable.js
@@ -0,0 +1,33 @@
+//num type variable
+var sunglassprice = 120.4323434;
+
+//string type varriable
+var specialname = "naeem islam";
+
+//boolean 
+var issilngle = true;
+
+//float=3.5
+//int=1
+
+//variable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or speace
+ * 3. no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+*/
+//operation
+
+// Math Operations: +, -, *, /
+// Shorthand: +=, -=, ++, --
+
+// convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder : vagses: %
+console.log(sunglassprice.toFixed(2))
