a22d583aa99e9ad4a949b49cd61958fd80fa5376
naeem <naeemislamjr182@gmail.com>
2023-08-01 02:00:38 +0600

    elseif

---
+++ multistageElseif.js
@@ -0,0 +1,17 @@
+var money = 8;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (money > danishPrice) {
+    console.log("danish kahbo akhon");
+}
+else if (money > butterBread) {
+    console.log("butterBread khsite moja")
+}
+else if (money > toastBiscuit) {
+    console.log("tostbisket khabo vijay vijay")
+}
+else {
+    console.log("cha e khabo tahale ghum asbe na")
+}
